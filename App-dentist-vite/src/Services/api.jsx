ba5d37dc3f166708c5661673b6e721c7a2ddd789
LeonardoHerna <95939325+LeonardoHerna@users.noreply.github.com>
2025-08-10 08:36:35 -0300

    Modicifacion de api en frontend

--- App-dentist-vite/src/Services/api.jsx
+++ App-dentist-vite/src/Services/api.jsx
@@ -5,6 +5,17 @@ const API = axios.create({
     import.meta.env.MODE === "production"
       ? import.meta.env.VITE_API_URL_PROD
       : import.meta.env.VITE_API_URL_DEV,
+       withCredentials: true
 });
+
+// Interceptor para agregar token en header Authorization en cada petición
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 
 export default API;
