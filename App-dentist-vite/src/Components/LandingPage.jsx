04db8956f0483ad63e672c2f662d592ed98bc298
Leonardo Hernandez Bentancor <leonardohernanes307@gmail.com>
2025-08-10 11:45:02 -0300

    Update LandingPage.jsx

--- App-dentist-vite/src/Components/LandingPage.jsx
+++ App-dentist-vite/src/Components/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "./Components/logo
-  jpg";
+import logo from "./Components/Logo.jpg";
 
 const LandingPage = () => {
   const [email, setEmail] = useState("");
